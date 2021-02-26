a87cd939f751039bc4abef72467c50f73fcac892
Carlos Laurine <carlos.pinho@ucsal.edu.com.br>
2021-02-25 23:04:45 -0300

    Board Printing + Set Chess Layer

---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,25 @@
+package chess;
+
+import board.Board;
+
+public class ChessMatch {
+	
+	private Board board;
+	
+	public ChessMatch() {
+		
+		board = new Board(8,8);
+	}
+	
+	public ChessPiece[][] getPieces(){
+		ChessPiece[][] matrix = new ChessPiece[board.getRows()][board.getColumns()];
+		for (int i=0; i<board.getRows(); i++) {
+			for(int j=0; j<board.getColumns(); j++) {
+				matrix[i][j] = (ChessPiece) board.piece(i,j);
+			}
+		}
+		return matrix;
+		
+	}
+
+}
