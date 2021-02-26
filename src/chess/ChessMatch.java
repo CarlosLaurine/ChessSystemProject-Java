05da6299c7f56ef9755494253cc1ed162a43abe4
Carlos Laurine <carlos.pinho@ucsal.edu.com.br>
2021-02-26 02:53:03 -0300

    King + Rook Created plus testing Piece Placing on the Board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import board.Board;
+import board.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -9,6 +12,7 @@ public class ChessMatch {
 	public ChessMatch() {
 		
 		board = new Board(8,8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -20,6 +24,14 @@ public class ChessMatch {
 		}
 		return matrix;
 		
+	}
+	
+	private void initialSetup() {
+		
+		
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7,4));
 	}
 
 }
