70bf432a9b02251c6a855b92e8816a100a7f63e8
Carlos Laurine <carlos.pinho@ucsal.edu.com.br>
2021-02-27 16:00:56 -0300

    Piece Movement Implemented w/Exceptions

--- src/board/Board.java
+++ src/board/Board.java
@@ -49,6 +49,19 @@ public class Board {
 		pieces [position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	public Piece removePiece (Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException ("Position doesn't exist on the Board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 	private boolean positionExists (int row, int column) {
 		boolean verif = row >= 0 && row < rows && column >= 0 && column < columns; 
 		return verif;
