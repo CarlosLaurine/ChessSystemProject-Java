716587bd00c8391b459db3801e6bf9a4273958b6
Carlos Laurine <carlos.pinho@ucsal.edu.com.br>
2021-03-17 03:40:08 -0300

    Special Move CASTLING 1/2 Implemented (Position Verification Methods and Conditionals at King Class + Castling Possibility Identification Logic + Constructor Parameters Modification at King and ChessMatch Classes (this ChessMatch Reference))

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,15 +2,18 @@ package chess.pieces;
 
 import board.Board;
 import board.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
-
-	public King(Board board, Color color) {
-		super(board, color);
-	}
 	
+	private ChessMatch chessMatch;
+
+	public King(Board board, Color color, ChessMatch chessMatch) {
+		super(board, color);
+		this.chessMatch = chessMatch;
+	}	
 	
 	@Override
 	public String toString() {
@@ -21,7 +24,14 @@ public class King extends ChessPiece {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
 	}
+	
+	private boolean testRookCastling(Position position) {
+		
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p!=null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
+	}
 
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [][] matrix = new boolean [getBoard().getRows()][getBoard().getColumns()];
@@ -77,8 +87,35 @@ public class King extends ChessPiece {
 		matrix[p.getRow()][p.getColumn()] = true;
 		}	
 		
-		
-		
+		//================ SPECIAL KING MOVEMENT "CASTLING"================
+		if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// KINGSIDE CASTLING ->> "SMALL CASTLING"
+			Position pR1 = new Position(position.getRow(), position.getColumn() + 3);
+			if(testRookCastling(pR1)) {
+					Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+					Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+					if(getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+					
+						matrix[position.getRow()][position.getColumn() + 2] = true;
+						
+					}
+
+			}
+			// QUEENSIDE CASTLING ->> "LONG CASTLING"
+						Position pR2 = new Position(position.getRow(), position.getColumn() -4);
+						if(testRookCastling(pR2)) {
+								Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+								Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+								Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+								if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+								
+									matrix[position.getRow()][position.getColumn() - 2] = true;
+									
+								}
+
+						}
+			
+		}
 		
 		
 		return matrix;
